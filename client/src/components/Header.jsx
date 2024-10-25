7c89f429168d985bdda510f07c3c5bb2f4b47f2a
Hye Mee Park <hyemee1021@gmail.com>
2024-10-25 14:27:32 +1100

    dark,light theme complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,21 @@ import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { IoMenuSharp } from "react-icons/io5";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { toggleLight } from "../store.js";
 export const Header = () => {
   const [showInput, setShowInput] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+
+  const dispatch = useDispatch();
+  const light = useSelector((state) => state.light.value);
+
+  const toggleMode = () => {
+    dispatch(toggleLight(light));
+  };
+  console.log("light" + light);
 
   const toggleInput = () => {
     setShowInput(!showInput);
@@ -15,8 +27,10 @@ export const Header = () => {
   };
   console.log(showInput);
   return (
-    <div className="border w-full relative">
-      <div className="w-[90%] sm:w-4/5 mx-auto  flex flex-row justify-between items-center text-gray-500  py-2">
+    <div className={`border w-full relative  `}>
+      <div
+        className={`w-[90%] sm:w-4/5 mx-auto flex flex-row justify-between items-center py-2 shadow-sm`}
+      >
         {/* logo */}
         <div className=" flex gap-2 items-center">
           <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-xl text-white font-bold text-xl py-1 px-2 ">
@@ -47,8 +61,8 @@ export const Header = () => {
           <IoMenuSharp className="cursor-pointer" onClick={toggleMenu} />
         </div>
         {showMenu && (
-          <div className="w-[50%] bg-slate-100 absolute top-[100%] right-0">
-            <ul className="flex flex-col  w-[50%] bg-slate-100 min-h-full py-5 gap-5 items-center  mx-auto">
+          <div className="w-[50%] bg-slate-50 absolute top-[100%] right-0 shadow-md">
+            <ul className="flex flex-col  w-[50%] bg-slate-50 min-h-full py-5 gap-5 items-center  mx-auto">
               <li className="cursor-pointer">Home</li>
               <li lassName="cursor-pointer">About</li>
               <li lassName="cursor-pointer">Projects</li>
@@ -57,7 +71,11 @@ export const Header = () => {
         )}
 
         <div>
-          <FaMoon />
+          {light ? (
+            <FaMoon onClick={toggleMode} />
+          ) : (
+            <FaSun onClick={toggleMode} />
+          )}
         </div>
       </div>
     </div>
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
+// redux
+import { useSelector } from "react-redux";
 export const Layout = () => {
+  const light = useSelector((state) => state.light.value);
   return (
-    <div className="w-[100%] mx-auto ">
+    <div
+      className={`w-full mx-auto ${
+        light ? "bg-slate-100 text-gray-500" : "bg-slate-800 text-white"
+      }`}
+    >
       <Header />
       <main>
         <Outlet />
